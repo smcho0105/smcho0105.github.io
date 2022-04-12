e5f9650b689642260a17d14069438a3f92963498
smcho0105 <103232672+smcho0105@users.noreply.github.com>
2022-04-12 10:50:46 +0900

    Add files via upload

---
+++ 04.shapes/sketch.js
@@ -0,0 +1,72 @@
+function setup() {
+  createCanvas(650, 300);
+  background(200);
+
+  // push(); // start a new drawing state
+  // strokeWeight(5);
+  // stroke(255, 0 ,0, 150);
+  // point(150, 120);
+
+  // //noStroke(); -> 뒤에 도형은 다 지워짐
+  // // 점과 선은 stroke로만 이루어짐
+  
+  // line(0, 0, 300, 300);
+  // pop(); // restore original state
+  // stroke(0, 0, 255, 150);
+  // strokeWeight(5);
+  // line(300, 0, 0, 300);
+  // point(150, 180);
+  
+  //push();
+  //CORNER, CORNERS, CENTER, OR RADIUS
+  // rectMode(CENTER);
+  // strokeWeight(3);
+  // stroke('red');
+  // rect(150, 150, 100, 100);
+  // pop();
+  // strokeWeight(2);
+  // stroke('black');
+  // ellipse(150, 150, 100, 100);
+  // fill(255, 0, 0, 150);
+  // rect(150, 150, 100, 100);
+
+
+  // push();
+  // strokeWeight(3);
+  // stroke('#5259E4');
+  // fill('#BF71F4');
+  // triangle(0, 0, 100, 100, 300, 0);
+  // pop();
+
+  // rectMode(CENTER);
+  
+  // //smooth();
+  // push();
+  // stroke('black');
+  // strokeWeight(3);
+  // rect(150, 150, 55, 55, 5);
+  // pop();
+
+
+  // strokeWeight(3);
+  // square(150, 250, 55, 3);
+
+  
+
+  // let a = 100;
+
+  // strokeWeight(3);
+  
+  // arc(a      , a + 50, a, a, PI, PI*3/2, PIE);
+  // arc(a + 150, a + 50, a, a, 0, PI, PIE);
+  // arc(a + 300, a + 50, a, a, 0, PI*3/2, PIE);
+  // arc(a + 450, a + 50, a, a, 0, PI*2, PIE);
+
+
+
+}
+
+function draw() {
+
+
+}
